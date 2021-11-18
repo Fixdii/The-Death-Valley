cc80faf0e976ddeadf2c0667c1b5663677382ed8
Fixdii <walera2001@gmail.com>
2021-11-18 14:19:08 +0300

    fix bugs and make adaptiv

--- script/script.js
+++ script/script.js
@@ -1,13 +1,19 @@
-const aboutPerson = document.querySelector('.rectangle__content');
-const triangle = document.querySelector('.team-part__triangle');
-const manCard = document.querySelector('.man-card');
+let modal = document.getElementById('myModal');
 
-manCard.addEventListener('mouseover', (event) => {
-  triangle.style.display = 'block';
-  aboutPerson.style.display = 'block';
+let img = document.querySelector('.works__cards');
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+
+img.addEventListener("click", (event) => {
+    console.log(event.target.src);
+    modal.style.display = "block";
+    modalImg.src = event.target.src;
 })
 
-manCard.addEventListener('mouseout', (event) => {
-  triangle.style.display = 'none';
-  aboutPerson.style.display = 'none';
-})
+let span = document.getElementsByClassName("close")[0];
+console.log(span);
+
+span.onclick = function() { 
+    modal.style.display = "none";
+    console.log(1)
+}
